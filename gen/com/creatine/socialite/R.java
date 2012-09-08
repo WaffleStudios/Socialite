92f1a9758640816d2111dcbd53c8d843d7788b4b
Rob Stemen <rob.stemen@gmail.com>
2012-09-07 21:28:55 -0500

    Fixed.  Git issues on my end.  I'm an idiot.

--- gen/com/creatine/socialite/R.java
+++ gen/com/creatine/socialite/R.java
@@ -27,16 +27,13 @@ public final class R {
         public static final int menu_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050004;
         public static final int menu_post=0x7f050001;
         public static final int menu_settings=0x7f050002;
         public static final int title_activity_main=0x7f050003;
