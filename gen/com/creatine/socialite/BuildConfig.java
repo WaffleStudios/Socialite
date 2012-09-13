527524aac1697bb8b676206cb16ef7502b507192
Rob Stemen <rob.stemen@gmail.com>
2012-09-12 19:49:53 -0500

    Added ability to post photos, general cleanup, framework to add photos from camera.

--- gen/com/creatine/socialite/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.creatine.socialite;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/creatine/socialite/Main.java
+++ src/com/creatine/socialite/Main.java
@@ -1,46 +1,47 @@
 package com.creatine.socialite;
 
-import java.io.IOException;
-
 import com.facebook.android.DialogError;
 import com.facebook.android.Facebook;
 import com.facebook.android.FacebookError;
 import com.facebook.android.Facebook.DialogListener;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 
 
-public class Main extends Activity {
+public class Main extends SherlockActivity {
 	
 	public static final String APP_ID = "223686664426439";
-	private static final String[] PERMISSIONS = { "publish_stream" };
+	private static final String[] PERMISSIONS = { "publish_stream", "user_photos" };
+	private static final String PREFS = "sharedPrefs";
 	
-	Facebook facebook = new Facebook(Main.APP_ID);
 	private SharedPreferences mPrefs;
+	private long expires;
+	String access_token;
+	Facebook facebook = new Facebook(APP_ID);
 	
-	
-	// Checks login, and logs in if necessary.
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.main);
-    	/*
-         * Get existing access_token if any
+        /*
+         * Gets existing access token, if any.
          */
-        mPrefs = getPreferences(MODE_PRIVATE);
-        String access_token = mPrefs.getString("access_token", null);
-        long expires = mPrefs.getLong("access_expires", 0);
+    	mPrefs = getSharedPreferences(PREFS, MODE_PRIVATE);
+        access_token = mPrefs.getString("access_token", null);
+        expires = mPrefs.getLong("access_expires", 0);
         if(access_token != null) {
             facebook.setAccessToken(access_token);
         }
         if(expires != 0) {
             facebook.setAccessExpires(expires);
         }
+        // If there is no existing access token, it will log in and save access token.
     	if(!facebook.isSessionValid()) {
     		facebook.authorize(this, Main.PERMISSIONS,
     				new DialogListener() {
@@ -59,17 +60,9 @@ public class Main extends Activity {
     	    	
     	    });
     	}
-    	Button test = (Button) findViewById(R.id.test_button);
-    	test.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	EditText edit = (EditText) findViewById(R.id.status_entry);
-            	String status = edit.getText().toString();
-            	updateStatus(status);
-            }
-        });
+    	
     }
-    
-    // Extends the access token, if necessary.
+
     public void onResume() {    
         super.onResume();
         facebook.extendAccessTokenIfNeeded(this, null);
@@ -79,20 +72,25 @@ public class Main extends Activity {
     	super.onActivityResult(requestCode, resultCode, data);
 
         facebook.authorizeCallback(requestCode, resultCode, data);
-    }	
-    
-    public void updateStatus(final String msg) {
-	    new Thread(new Runnable() {
-	    	public void run() {
-				Bundle parameters = new Bundle();
-				parameters.putString("message", msg);
-				try {
-					String response = facebook.request("me/feed", parameters,"POST");
-					System.out.println(response);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-	    	}
-	    }).start();
+    }
+    /*
+     * Menu options
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuinflate = new MenuInflater(this);
+		menuinflate.inflate(R.menu.main, menu);
+		return true;
+	}	
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    if (item.getItemId() == R.id.menu_status) {
+	    	Intent p = new Intent(this, Status.class);
+	    	p.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	    	startActivity(p);
+	    	return true;
+	    }
+		return false;
 	}
 }
+
