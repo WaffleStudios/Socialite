a1e6ea28e222279634be07b2c5b49e00a4245c45
Rob Stemen <rob.stemen@gmail.com>
2012-09-12 20:08:42 -0500

    Fixing more stuff.  Please forgive me my git errors.

---
+++ src/com/creatine/socialite/FBPost.java
@@ -0,0 +1,60 @@
+package com.creatine.socialite;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap.CompressFormat;
+import android.os.Bundle;
+
+import com.facebook.android.Facebook;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+
+
+public class FBPost {
+	
+	private Facebook facebook;
+	
+	public FBPost(String appId, String access_token) {
+		facebook = new Facebook(appId);
+		facebook.setAccessToken(access_token);
+	}
+	
+	public void updateStatus(final String status) {
+		new Thread(new Runnable () {
+			public void run() {
+				Bundle parameters = new Bundle();
+				parameters.putString("message", status);  
+				try {
+					String response = facebook.request("me/feed", parameters,"POST");
+					System.out.println(response);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		
+	}
+	
+	public void postPhoto(final String imagePath, final String status) {
+		new Thread(new Runnable () {
+	    	public void run() {
+	    		Bundle parameters = new Bundle();
+	    		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	    		Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+	    	    bitmap.compress(CompressFormat.JPEG, 100, bos);
+	    	    if(status != null) {
+	    	    	parameters.putString("message", status);
+	    	    }
+	    	    parameters.putByteArray("picture", bos.toByteArray());
+	    		try {
+	    			String response = facebook.request("me/photos", parameters,"POST");
+	    			System.out.println(response);
+	    		} catch (IOException e) {
+	    			e.printStackTrace();
+	    		}
+	    	}
+	    }).start();
+	}
+}
---
+++ src/com/creatine/socialite/Status.java
@@ -0,0 +1,120 @@
+package com.creatine.socialite;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
+import java.io.File;
+
+public class Status extends SherlockActivity {
+	
+	private static final int SELECT_PICTURE = 1;
+	private static final int CAMERA_PICTURE = 2;
+	
+	private FBPost post;
+	private SharedPreferences mPrefs;
+	private String selectedImagePath;
+	private String access_token;
+	private Uri imageUri;
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.status);
+		ActionBar actionBar = getSupportActionBar();
+	    actionBar.setDisplayHomeAsUpEnabled(true);
+		mPrefs = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+		access_token = mPrefs.getString("access_token", null);
+		post = new FBPost(Main.APP_ID, access_token);
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		/*
+		 * Gets picture path based on method of entry
+		 */
+		if (requestCode == SELECT_PICTURE) {
+            if(data != null) {
+            	Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+            }
+        }
+	    if (resultCode == CAMERA_PICTURE) {
+	            Uri selectedImageUri = data.getData();
+	            selectedImagePath = getPath(selectedImageUri);
+	    }
+	}
+	// Retrieves path from Uri to allow for upload
+	public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+		Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+	// Creates menu
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuinflate = new MenuInflater(this);
+		menuinflate.inflate(R.menu.status, menu);
+		return true;
+	}	
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.post_status) {
+	    	EditText edit = (EditText) findViewById(R.id.status_entry);
+        	String status = edit.getText().toString();
+        	if(selectedImagePath != null) {
+        		post.postPhoto(selectedImagePath, status);
+        		selectedImagePath = null;
+        	} else {
+        		post.updateStatus(status);
+        	}
+        	Intent home = new Intent(this, Main.class);
+    		home.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    		startActivity(home);
+    		Context context = getApplicationContext();
+    		int duration = Toast.LENGTH_SHORT;
+    		Toast toast = Toast.makeText(context, "Successfully posted", duration);
+    		toast.show();
+	    	return true;
+	    }
+		if (item.getItemId() == R.id.add_photo) {
+			Intent selectPhoto = new Intent(Intent.ACTION_PICK,
+		               android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			startActivityForResult(selectPhoto, SELECT_PICTURE);
+			return true;
+		}
+	    if (item.getItemId() == R.id.take_photo) {
+	    	Intent cameraIntent = new Intent("android.media.action.IMAGE_CAPTURE");
+	    	File photo = new File(Environment.getExternalStorageDirectory(),  "pic.jpg");
+	    	cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+	                Uri.fromFile(photo));
+	    	imageUri = Uri.fromFile(photo);
+	    	selectedImagePath = getPath(imageUri);
+            return true;
+	    }
+		switch (item.getItemId()) {
+        case android.R.id.home:
+            Intent intent = new Intent(this, Main.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+		}
+	}
+}
